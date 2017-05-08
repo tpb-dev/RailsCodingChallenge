2666f45b9faa14a07e4768f3db39690e63f63e98
Randall Tom <randall.tom23@gmail.com>
2017-05-08 09:20:32 -0700

    2nd Commit

--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,37 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
+  describe "init" do
+    it "init a cuboid" do
+        cub = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
+        expect(cub).to be_a Cuboid
+      end
+    it "raises error if invalid sides" do
+      expect{Cuboid.new([0, 0, 0], h:0, w:0, l:0 )}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "move" do
     it "changes the origin in the simple happy case" do
       expect(subject.move_to!(1,2,3)).to be true
     end
-  end    
-  
+  end
+
   describe "intersects?" do
+    it "intesects correctly" do
+      cub1 = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
+      cub2 = Cuboid.new([0, 0, 0], h:1, w:2, l:5 )
+      expect(cub1.intersects?(cub2)).to be(true)
+    end
+
+    it "does not intersect" do
+      cub3 = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
+      cub4 = Cuboid.new([10, 10, 10], h:10, w:20, l:50 )
+      expect(cub3.intersects?(cub4)).to be(false)
+    end
   end
 
 end
