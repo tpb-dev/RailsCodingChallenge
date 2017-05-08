247973096d543d765e504af0ac3b6868d57bce3a
Randall Tom <randall.tom23@gmail.com>
2017-05-08 10:06:47 -0700

    3rd Commit

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,26 +1,34 @@
 
 class Cuboid
-  attr_reader :x, :y, :z, :vert_x, :vert_y, :vert_z, :x_outer, :y_outer, :z_outer
+  attr_reader :x, :y, :z, :l, :w, :h, :x_outer, :y_outer, :z_outer
   #BEGIN public methods that should be your starting point
-  def initialize(a,b,c)
-    @x, @y, @z = a, b, c
+  def initialize(orA, orB, orC, a,b,c)
+    @x, @y, @z = orA, orB, orC
+    @l, @w, @h = a, b, c
     if (a <= 0 || b<=0 || c<=0)
       raise ArgumentError.new("Sides can not be 0")
-    move_to(0, 0, 0)
+    end
+  end
 
   def move_to!(x, y, z)
-    @vert_x, @vert_y, @vert_z = x, y, z
-    @x_outer, @y_outer, @z_outer = @x_outer + @x, @y_outer + @y, @z_outer + @z
+    @x, @y, @z = x, y, z
+    return true
   end
 
   def vertices
-    @vert_x, @vert_y, @vert_z = 0, 0, 0
+    @x, @y, @z = 0, 0, 0
   end
 
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
-    (@x <= other.x_outer && other.x <= @x_outer) & (@y <= other.y_outer && other.y <= @y_outer) &
-    (@z <= other.z_outer && other.z <= @z_outer)
+#    (@x <= other.x_outer && other.x <= @x_outer) || (@y <= other.y_outer && other.y <= @y_outer) ||
+#    (@z <= other.z_outer && other.z <= @z_outer)
+
+
+    (other.x < (@x + @l)) && ((other.x + other.l) > @x) &&
+    (other.y < (@y + @w)) && ((other.y + other.w) > @y) &&
+    (other.z < (@z + @h)) && ((other.z + other.h) > @z)
+
   end
 
   #END public methods that should be your starting point
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -6,30 +6,31 @@ require 'cuboid'
 describe Cuboid do
   describe "init" do
     it "init a cuboid" do
-        cub = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
+        cub = Cuboid.new(0, 0, 0, 1, 2, 3 )
         expect(cub).to be_a Cuboid
       end
     it "raises error if invalid sides" do
-      expect{Cuboid.new([0, 0, 0], h:0, w:0, l:0 )}.to raise_error(ArgumentError)
+      expect{Cuboid.new(0, 0, 0, 0, 0, 0 )}.to raise_error(ArgumentError)
     end
   end
 
   describe "move" do
     it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+      cub0 = Cuboid.new(0, 0, 0, 1, 2, 3 )
+      expect(cub0.move_to!(1,2,3)).to be true
     end
   end
 
   describe "intersects?" do
     it "intesects correctly" do
-      cub1 = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
-      cub2 = Cuboid.new([0, 0, 0], h:1, w:2, l:5 )
+      cub1 = Cuboid.new(0, 0, 0, 1, 2, 3 )
+      cub2 = Cuboid.new(0, 0, 0, 1, 2, 4 )
       expect(cub1.intersects?(cub2)).to be(true)
     end
 
     it "does not intersect" do
-      cub3 = Cuboid.new([0, 0, 0], h:1, w:2, l:3 )
-      cub4 = Cuboid.new([10, 10, 10], h:10, w:20, l:50 )
+      cub3 = Cuboid.new(6, 6, 6, 6, 6, 6 )
+      cub4 = Cuboid.new(0, 0, 0, 1, 1, 1 )
       expect(cub3.intersects?(cub4)).to be(false)
     end
   end
